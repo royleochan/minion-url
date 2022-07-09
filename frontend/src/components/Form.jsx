87efff4f674080548aeb05d320258bbc6967d99f
Roy Chan <royleochan@gmail.com>
2022-07-09 15:03:47 +0800

    cleanup

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -7,9 +7,6 @@ import {
   Button,
 } from "@chakra-ui/react";
 
-const URL_REGEX =
-  "https?://(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)";
-
 const Form = () => {
   const {
     handleSubmit,
