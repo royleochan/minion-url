92866d153f0f972375190153703377be4f43dde5
Roy Chan <royleochan@gmail.com>
2022-07-09 18:55:43 +0800

    fix: cors error

--- backend/app/url_shortener.py
+++ backend/app/url_shortener.py
@@ -15,15 +15,10 @@ url.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
-
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
