e7e13f7d580b4c789d4f2e572fddfb759d26bdbf
Roy Chan <royleochan@gmail.com>
2022-07-09 13:10:22 +0800

    feat: add axios client

---
+++ frontend/src/utils/request.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8000/",
+  timeout: 20000,
+});
+
+const request = {
+  get: (url, settings) => {
+    return api.get(url, settings);
+  },
+  post: (url, data) => {
+    return api.post(url, data);
+  },
+};
+
+export default request;
