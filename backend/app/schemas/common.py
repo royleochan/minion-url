b0f5e96768d7329345d47f3dc65b34c9d2b9762b
Roy Chan <royleochan@gmail.com>
2022-07-09 15:11:06 +0800

    refactor: use camel case in api

--- backend/app/schemas/common.py
+++ backend/app/schemas/common.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from humps import camelize
 
 
 class ApiBase(BaseModel):
     class Config:
+        alias_generator = camelize
         orm_mode = True
         allow_population_by_field_name = True
